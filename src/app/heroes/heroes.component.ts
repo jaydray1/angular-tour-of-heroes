b2253523e57dce295cfe50414326a5b9a2938983
JM <jdreher15@gmail.com>
2018-03-16 10:58:26 -0600

    stuff

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+ 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-
+ 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,21 +10,30 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-
-  // selectedHero: Hero;
-
-  constructor(private heroService: HeroService) {}
-
+ 
+  constructor(private heroService: HeroService) { }
+ 
   ngOnInit() {
     this.getHeroes();
   }
-  // onSelect(hero: Hero): void {
-  //   this.selectedHero = hero;
-  // }
+ 
   getHeroes(): void {
     this.heroService.getHeroes()
-    // subscribe passes the emitted array to the callback, which sets the 
-    // components heroes property
-      .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
+ 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+ 
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+ 
 }
